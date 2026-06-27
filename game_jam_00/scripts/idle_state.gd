665d41e8fe99e59df1de28609891381864d8eab8
Ilai Van Der Veldt <ivan-der@f1r4s6.codam.nl>
2026-06-27 15:38:12 +0200

    added player stuff, lighting and window settings for pixel perfection

---
+++ game_jam_00/scripts/idle_state.gd
@@ -0,0 +1,8 @@
+extends State
+
+
+func on_enter():
+	pass
+
+func state_process(_delta):
+	pass
--- game_jam_00/scripts/player.gd
+++ game_jam_00/scripts/player.gd
@@ -1,16 +1,27 @@
 extends CharacterBody2D
 
-const SPEED = 125.0
+class_name Player
 
-var direction: Vector2
- 
+@export var p_sprite: Sprite2D
+@export var state_machine: CharacterStateMachine
+
+const SPEED: float = 42.0
+
+#var can_move: bool = state_machine.current_state.can_move
+var dir: Vector2
+var prev_dir: Vector2
 
 func _physics_process(delta: float) -> void:
 	#Direction input and movement handling
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.y = Input.get_axis("move_up", "move_down")
-	if direction:
-		velocity = lerp(velocity, direction * SPEED, delta * 2)
+	dir.x = Input.get_axis("move_left", "move_right")
+	dir.y = Input.get_axis("move_up", "move_down")
+	if dir:
+		velocity = lerp(velocity, dir * SPEED, delta * 2)
+		if prev_dir.x > 0:
+			p_sprite.flip_h = false
+		elif prev_dir.x < 0:
+			p_sprite.flip_h = true
+		prev_dir = dir
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, delta * 10)
 	move_and_slide()
