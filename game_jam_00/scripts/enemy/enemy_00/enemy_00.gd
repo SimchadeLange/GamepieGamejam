73e1e792179fd3009011cee2c6a998b50366702c
Ilaivdv <ilaivanderveldt@gmail.com>
2026-06-29 12:06:39 +0200

    integrated movement component into scenes, and navigation works  now

--- game_jam_00/scripts/enemy/enemy_00/enemy_00.gd
+++ game_jam_00/scripts/enemy/enemy_00/enemy_00.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-const SPEED: float = 20.0
-
 @export var state_machine: CharacterStateMachine
 @export var movement_component: MovementComponent
+@export var pathfinding_component: PathfindingComponent
 @export var health_component: HealthComponent
 @export var sprite: Sprite2D
 
@@ -12,6 +11,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	movement_component.movement_process(delta)
+	pathfinding_component.chase_target()
 	
 	if movement_component.prev_dir.x > 0:
 		sprite.flip_h = false
--- game_jam_00/scripts/resources/pathfinding_component.gd
+++ game_jam_00/scripts/resources/pathfinding_component.gd
@@ -1,5 +1,7 @@
 extends NavigationAgent2D
 
+class_name PathfindingComponent
+
 @export var character: PhysicsBody2D
 @export var movement_component: MovementComponent
 
@@ -7,6 +9,7 @@ func _ready() -> void:
 	await Globals.has_player
 	target_position = Globals.player.global_position
 
-func _physics_process(_delta: float) -> void:
+func chase_target() -> void:
+	target_position = Globals.player.global_position
 	if !is_target_reached():
-		movement_component.dir = character.to_local(get_next_path_position().normalized())
+		movement_component.dir = character.to_local(get_next_path_position()).normalized()
